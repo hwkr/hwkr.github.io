b92c2fcc27053143c20b62cc87e600446d328d4a
Brynn Hawker <brynn@hawker.me>
2016-12-10 18:27:21 -0800

    Add chat window

--- components/pages/Home.jsx
+++ components/pages/Home.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import Config from 'Config'
+
 import Icon from 'parts/Icon';
 
 export default class Home extends Component {
@@ -15,6 +17,10 @@ export default class Home extends Component {
   // static defaultProps = {
   // }
 
+  openChat = () => {
+    window.open(Config.chat_url, 'newwindow', 'width=500, height=600'); return false;
+  }
+
   render() {
     return (
       <div className="home">
@@ -28,7 +34,7 @@ export default class Home extends Component {
               </small>
             </h1>
             <br />
-            <button className="btn btn-primary btn-fat">
+            <button className="btn btn-primary btn-fat" onClick={this.openChat}>
               <Icon name="social-facebook-messenger-bubble" size={1.3} /> Talk to me right meow
             </button>
           </div>
